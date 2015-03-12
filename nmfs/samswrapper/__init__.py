04d64f4eb841ed7636f5137db9d0b2ed4f1aa5fe
John Evans <john.g.evans@noaa.gov>
2015-03-12 08:55:30 -0400

    Remove Qt from samswrapper
    No longer need the Qt gui client.
    Closes-Issue:  #9

--- nmfs/samswrapper/__init__.py
+++ nmfs/samswrapper/__init__.py
@@ -1,13 +1,10 @@
 import os
 
+import matplotlib                                                              
+matplotlib.use('Agg')                                                          
+
 from . import core
 from .samswrapper import SamsWrapper
 from . import command_line
 
-if 'DJANGO_SETTINGS_MODULE' in os.environ:                                         
-    import matplotlib                                                              
-    matplotlib.use('Agg')                                                          
-    __all__ = [core, SamsWrapper, command_line]                                    
-else:                                                                              
-    from . import gui                                                              
-    __all__ = [core, SamsWrapper, command_line, gui] 
+__all__ = [core, SamsWrapper, command_line]                                    
