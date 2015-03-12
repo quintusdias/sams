cf2f91e3b6b5e309f7b4a99440399ba7cdf5ee00
John Evans <john.g.evans@noaa.gov>
2015-03-12 09:12:53 -0400

    Refactor unittests test configurations
    Three existing configurations tested, but two of those tests can re-use
    much of the setup performed by the third.
    
    Closes-Issue:  #5

--- nmfs/samsweb/tests.py
+++ nmfs/samsweb/tests.py
@@ -84,82 +84,17 @@ class SamsTests(TestCase):
         return(data)
 
     def generate_elephand_trunk_closed_extended(self):
-        data = {'ma_row_0_col_0': '0.02',
-                'ma_row_1_col_0': '0.35',
-                'ma_row_2_col_0': '0.45',
-                'ma_row_3_col_0': '0.55',
-                'ma_row_0_col_2': '0.00',
-                'ma_row_1_col_2': '0.35',
-                'ma_row_2_col_2': '0.45',
-                'ma_row_3_col_2': '0.55',
-                'ma_row_0_col_3': '0.00',
-                'ma_row_1_col_3': '0.05',
-                'ma_row_2_col_3': '0.00',
-                'ma_row_3_col_3': '0.35',
-                'ma_row_4_col_3': '0.45',
-                'ma_row_5_col_3': '0.55',
-                'ma_row_0_col_4': '0.40',
-                'ma_row_1_col_4': '0.35',
-                'ma_row_2_col_4': '0.45',
-                'ma_row_3_col_4': '0.55'}
+        data = self.generate_default_post_params()
 
-        data['ma_sub_area_active_0'] = 'on'
-        data['ma_sub_area_active_2'] = 'on'
-        data['ma_sub_area_active_3'] = 'on'
-        data['ma_sub_area_active_4'] = 'on'
+        # Shift "Elephant Trunk Closed" values by two years forward.
+        # Zero-fill the first two year.
 
-        sub_region_names = ['Hudson Canyon',
-                            'Virginia Beach',
-                            'Elephant Trunk Open',
-                            'Elephant Trunk Closed',
-                            'DelMarVa',
-                            'New York Bight',
-                            'Long Island',
-                            'Inshore NYB']
-        post_format = ','.join([sname for sname in sub_region_names])
-        data['ma_sub_region_names'] = [post_format]
-
-        sub_region_names = ['CA1-NA', 'CA1-Acc', 'CA2-NA',  'CA2-Acc',
-                            'NLS-NA', 'NLS-Acc', 'CA2-Ext', 'NLS-Ext',
-                            'Sch',    'NEP',     'SEP']
-        post_format = ','.join([sname for sname in sub_region_names])
-        data['gb_sub_region_names'] = [post_format]
-
-        # CA1_Acc
-        data['gb_sub_area_active_1'] = 'on'
-        mortality = [0.02, 0.00, 0.00, 0.00, 0.00, 0.25, 0.25, 0.25, 0.25,
-                     0.25, 0.25, 0.25, 0.25, 0.25, 0.25]
-        for row, value in enumerate(mortality):
-            key = 'gb_row_{0}_col_1'.format(row)
-            data[key] = value
-
-        # CA2_Acc
-        data['gb_sub_area_active_3'] = 'on'
-        mortality = [0.60, 0.00, 0.30, 0.40, 0.50, 0.25, 0.25, 0.25, 0.25,
-                     0.25, 0.25, 0.25, 0.25, 0.25, 0.25]
-        for row, value in enumerate(mortality):
-            key = 'gb_row_{0}_col_3'.format(row)
-            data[key] = value
-
-        # NLS-Acc
-        data['gb_sub_area_active_5'] = 'on'
-        mortality = [0.80, 0.00, 0.00, 0.30, 0.40, 0.25, 0.25, 0.25, 0.25,
-                     0.25, 0.25, 0.25, 0.25, 0.25, 0.25]
-        for row, value in enumerate(mortality):
-            key = 'gb_row_{0}_col_5'.format(row)
-            data[key] = value
-
-        data['start_year'] = 2014
-        data['num_years'] = 15
-        data['open_area_f_mortality'] = 0.48
-
-        data['ma_natural_mortality'] = 0.20
-        data['ma_discard_mortality'] = 0.20
-        data['ma_incidental_mortality'] = 0.10
-
-        data['gb_natural_mortality'] = 0.16
-        data['gb_discard_mortality'] = 0.20
-        data['gb_incidental_mortality'] = 0.20
+        data['ma_row_0_col_3'] = '0.00'
+        data['ma_row_1_col_3'] = '0.00'
+        data['ma_row_2_col_3'] = '0.00'
+        data['ma_row_3_col_3'] = '0.35'
+        data['ma_row_4_col_3'] = '0.45'
+        data['ma_row_5_col_3'] = '0.55'
 
         return(data)
 
@@ -167,44 +102,9 @@ class SamsTests(TestCase):
         """
         First six sub areas in Georges Bank are active.
         """
-        data = {'ma_row_0_col_0': '0.02',
-                'ma_row_1_col_0': '0.35',
-                'ma_row_2_col_0': '0.45',
-                'ma_row_3_col_0': '0.55',
-                'ma_row_0_col_2': '0.00',
-                'ma_row_1_col_2': '0.35',
-                'ma_row_2_col_2': '0.45',
-                'ma_row_3_col_2': '0.55',
-                'ma_row_0_col_3': '0.00',
-                'ma_row_1_col_3': '0.35',
-                'ma_row_2_col_3': '0.45',
-                'ma_row_3_col_3': '0.55',
-                'ma_row_0_col_4': '0.40',
-                'ma_row_1_col_4': '0.35',
-                'ma_row_2_col_4': '0.45',
-                'ma_row_3_col_4': '0.55'}
+        data = self.generate_default_post_params()
 
-        data['ma_sub_area_active_0'] = 'on'
-        data['ma_sub_area_active_2'] = 'on'
-        data['ma_sub_area_active_3'] = 'on'
-        data['ma_sub_area_active_4'] = 'on'
-
-        sub_region_names = ['Hudson Canyon',
-                            'Virginia Beach',
-                            'Elephant Trunk Open',
-                            'Elephant Trunk Closed',
-                            'DelMarVa',
-                            'New York Bight',
-                            'Long Island',
-                            'Inshore NYB']
-        post_format = ','.join([sname for sname in sub_region_names])
-        data['ma_sub_region_names'] = [post_format]
-
-        sub_region_names = ['CA1-NA', 'CA1-Acc', 'CA2-NA',  'CA2-Acc',
-                            'NLS-NA', 'NLS-Acc', 'CA2-Ext', 'NLS-Ext',
-                            'Sch',    'NEP',     'SEP']
-        post_format = ','.join([sname for sname in sub_region_names])
-        data['gb_sub_region_names'] = [post_format]
+        # Add column 0, 2, 4 to the default scheme
 
         # CA1_NA
         data['gb_sub_area_active_0'] = 'on'
@@ -212,14 +112,6 @@ class SamsTests(TestCase):
                      0.25, 0.25, 0.25, 0.25, 0.25, 0.25]
         for row, value in enumerate(mortality):
             key = 'gb_row_{0}_col_0'.format(row)
-            data[key] = value
-
-        # CA1_Acc
-        data['gb_sub_area_active_1'] = 'on'
-        mortality = [0.02, 0.00, 0.00, 0.00, 0.00, 0.25, 0.25, 0.25, 0.25,
-                     0.25, 0.25, 0.25, 0.25, 0.25, 0.25]
-        for row, value in enumerate(mortality):
-            key = 'gb_row_{0}_col_1'.format(row)
             data[key] = value
 
         # CA2_NA
@@ -230,14 +122,6 @@ class SamsTests(TestCase):
             key = 'gb_row_{0}_col_2'.format(row)
             data[key] = value
 
-        # CA2_Acc
-        data['gb_sub_area_active_3'] = 'on'
-        mortality = [0.60, 0.00, 0.30, 0.40, 0.50, 0.25, 0.25, 0.25, 0.25,
-                     0.25, 0.25, 0.25, 0.25, 0.25, 0.25]
-        for row, value in enumerate(mortality):
-            key = 'gb_row_{0}_col_3'.format(row)
-            data[key] = value
-
         # NLS-NA
         data['gb_sub_area_active_4'] = 'on'
         mortality = [0.80, 0.00, 0.00, 0.30, 0.40, 0.25, 0.25, 0.25, 0.25,
@@ -245,26 +129,6 @@ class SamsTests(TestCase):
         for row, value in enumerate(mortality):
             key = 'gb_row_{0}_col_4'.format(row)
             data[key] = value
-
-        # NLS-Acc
-        data['gb_sub_area_active_5'] = 'on'
-        mortality = [0.80, 0.00, 0.00, 0.30, 0.40, 0.25, 0.25, 0.25, 0.25,
-                     0.25, 0.25, 0.25, 0.25, 0.25, 0.25]
-        for row, value in enumerate(mortality):
-            key = 'gb_row_{0}_col_5'.format(row)
-            data[key] = value
-
-        data['start_year'] = 2014
-        data['num_years'] = 15
-        data['open_area_f_mortality'] = 0.48
-
-        data['ma_natural_mortality'] = 0.20
-        data['ma_discard_mortality'] = 0.20
-        data['ma_incidental_mortality'] = 0.10
-
-        data['gb_natural_mortality'] = 0.16
-        data['gb_discard_mortality'] = 0.20
-        data['gb_incidental_mortality'] = 0.20
 
         return(data)
 
